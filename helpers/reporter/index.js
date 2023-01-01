ddda494250853aeb28fa2fae535134024bc8c0c1
ali aslani <67073328+ialiaslani@users.noreply.github.com>
2023-01-01 16:58:13 +0330

    Update index.js

--- helpers/reporter/index.js
+++ helpers/reporter/index.js
@@ -1,13 +1,23 @@
+const fs = require('fs')
+
 class CustomReporter {
 
     onRunComplete(testContexts, results) {
+        let text = 'Reporter At ' + new Date().toLocaleDateString("fa") + ': \n'
         console.log('Custom reporter output:');
         results.testResults.map(i =>   i.testResults).forEach((arr, index) => {
                 console.log('Scenario '+ ( index + 1 ) + ' : ', arr[0].fullName.replace(arr[0].title, ""))
+                text += 'Scenario '+ ( index + 1 ) + ' : ' + arr[0].fullName.replace(arr[0].title, "") + ' \n'
                 arr.forEach((item, i) => {
                     console.log(('Test '+ ( i + 1 ) + ' : ' + item.title + " = " + item.status))
+                    text += ('Test '+ ( i + 1 ) + ' : ' + item.title + " = " + item.status) + ' \n'
                 })
         })
+        try {
+            fs.writeFile(`./${'Reporter-At-' + new Date().getTime()}.txt`, text, () => {})
+        } catch (e) {
+            console.log("riteFile", e)
+        }
     }
 
 }
